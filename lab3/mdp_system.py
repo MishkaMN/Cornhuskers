981b26d903346f5b3ae0fe83a3a2a4e18195d448
Ayush Rath <ayush.rath@gmail.com>
2019-02-16 02:25:34 -0800

    Change state space
    refactored significant portion of code, added heading to state

--- lab3/mdp_system.py
+++ lab3/mdp_system.py
@@ -6,48 +6,49 @@ RIGHT = headings[2:5]
 DOWN = headings[5:8]
 LEFT = headings[8:11]
 
-STAY = 0
-FORWARD = 1
-BACKWARD = 2
-
-CLOCKWISE = -1
-CCLOCKWISE = -2
-
 # Test
-W = 5
+W = 6
 L = 6
-rewards = [1]*W*L
+rewards = np.flipud(np.array([
+    [-100, -100, -100, -100, -100, -100], 
+    [-100,    0,    0,  -10,    1, -100],
+    [-100,    0,    0,  -10,    0, -100],
+    [-100,    0,    0,    0,    0, -100],
+    [-100,    0,    0,    0,    0, -100],
+    [-100, -100, -100, -100, -100, -100]
+]));
 
 class State:
-    def __init__(self, x, y, row, col, reward):
+    def __init__(self, x, y, h, reward):
         # x: x coordinate in the grid-view
         # y: y coordinate in the grid-view
         self.x = x
         self.y = y
+        self.h = h
         self.reward = reward
         #
-        self.iden = self.x+self.y*L
+        self.iden = self.x+self.y*W
 
         # arrayIdx
     def __repr__(self):
-        return "State(grid_x: {}, grid_y: {}, reward: {}, id: {})".format(self.x, self.y, self.reward, self.iden)
+        return "State(grid_x: {}, grid_y: {}, grid_head: {} reward: {}, id: {})".format(self.x, self.y, self.h, self.reward, self.iden)
 
     def __str__(self):
-        return "({}, {})".format(self.x, self.y)
+        return "({}, {}, {})".format(self.x, self.y, self.h)
 
 class Action:
     STAY = 0
-    UP = 1
-    RIGHT = 2
-    DOWN = 3
-    LEFT = 4
-    actions = [STAY, UP, RIGHT, DOWN, LEFT]
-
-class StateTransitionBlock:
-    def __init__(self, state, next_state, action):
-        self.state = state
-        self.next_state = next_state
-        self.action = action
+    FORWARD_NOROT = 1
+    FORWARD_CLK = 2
+    FORWARD_CCLK = 3
+    BACKWARD_NOROT = 4
+    BACKWARD_CLK = 5
+    BACKWARD_CCLK = 6
+    fw_actions = [FORWARD_NOROT, FORWARD_CLK, FORWARD_CCLK]
+    bw_actions = [BACKWARD_NOROT, BACKWARD_CLK, BACKWARD_CCLK]
+    clk_actions = [FORWARD_CLK, BACKWARD_CLK];
+    cclk_actions = [FORWARD_CCLK, BACKWARD_CCLK];
+    actions = [STAY, FORWARD_NOROT, FORWARD_CLK, FORWARD_CCLK, BACKWARD_NOROT, BACKWARD_CLK, BACKWARD_CCLK]
 
 class Environment:
     def __init__(self, W, L, rewards):
@@ -55,22 +56,21 @@ class Environment:
         self.L = L
         self.robot = Robot(0, 0, 0, 0.1)
         # states in the environment in array-view
-        self.states = [[State(x, W-1-y, y, x, rewards[y*self.W+x]) 
-            for x in range(self.L)] for y in range(self.W)]
+        self.states = [[[State(x, y, h, rewards[y][x]) 
+             for x in range(self.L)] for y in range(self.W)] for h in headings]
 
     def printEnv(self):
-        for y in range(self.W):
+        tmp = self.states[self.robot.heading][:][:]
+        for y in reversed(range(self.W)):
             line = ''
             for x in range(self.L):
-                state = self.states[y][x]
+                state = tmp[y][x]
                 line += str(state)
                 if state.x == self.robot.x and state.y == self.robot.y:
                     line += '(R)'
                 else:
                     line += '   '
-                
             print(line)
-            print('\n')
 
     def step(self, translate, rotate):
         # translate: either move forwards or backwards
@@ -88,17 +88,30 @@ class Environment:
 
     def checkLoc(x, y):
         # check whether x and y (in grid view) are traversable
-        pass
+        if (x < 0 or x >= L or y < 0 or y >= W):
+            return False
+        else:
+            return True
 
     def flattenStates(self):
         flatten = []
-        for col in reversed(self.states):
-            flatten += col
+        for i in self.states:
+            for j in i:
+                flatten += j
         return flatten
 
-    def stateAt(self, x, y):
+    def stateAt(self, x, y, h):
         # return the state at x and y (in grid view)
-        return self.states[W-1-y][x] 
+        return self.states[h][y][x] 
+    
+    def next_state(self, a):
+        return self.robot.next_state_help(a, self.flattenStates());
+
+    def get_reward_at(self, x, y, h):
+        return self.stateAt(x,y,h).reward
+    
+    def get_reward_at(self, s_prime):
+        return self.stateAt(s_prime.x,s_prime.y,s_prime.h).reward
 
 class Robot:
     def __init__(self, x, y, heading, p_e):
@@ -115,12 +128,16 @@ class Robot:
         # prerotate can only possibly happen when the robot
         # moves
         dice = np.random.choice(3, 1, 
-                p=[self.p_e, self.p_e, 1 - 2*self.p_2])[0]
+                p=[self.p_e, self.p_e, 1 - 2*self.p_e])[0]
 
         if dice == 0:
             self.heading += 1
+            if self.heading > 11:
+                self.heading = 0
         elif dice == 1:
             self.heading -= 1
+            if self.heading < 0:
+                self.heading = 11
         
     def attempt_move(self, translate):
         if translate == STAY:
@@ -157,32 +174,87 @@ class Robot:
             raise ValueError('Invalid rotation')
 
     def move(self, x, y):
-        if abs(self.x-x) + abs(self.y-y) > 1:
+        if x < 0 or x >=L or y < 0 or y>=W:
             raise ValueError('Robot cannot move this far')
         self.x = x
         self.y = y
- 
-def transition_prob(p_e, s, s_prime, a):
-    # p_e: numpy 3D matrix containing state transition probabilities
-    # layout is using grid-view indices (i.e. state.iden)
-    return p_e[s.iden][s_prime.iden][a]
 
-def get_next_state(p_e, s, a):
-    possible_next_states = p_e[s.iden, :, a]
-    
-    # pick next state based on probabilities
-    return np.random.choice(possible_next_states)
+    def get_p(self, a, s_new):
+        #100 percent chance to stay in current spot
+        if a == Action.STAY:
+            if s_new.x == self.x and s_new.y == self.y and s_new.h and self.heading:
+                return 1
+            else:
+                return 0
+
+        #If robot moves
+        else:
+            #prerotation chances
+            chance = np.array([self.p_e, 1.0 - 2.0*self.p_e, self.p_e])
+            head = np.array([(self.heading - 1) % 12, self.heading, (self.heading + 1) % 12])
+
+            #get candidate states by x,y
+            s_primes = np.zeros([3,3])
+            for idx,h in enumerate(head):
+                if h in UP:
+                    if a in Action.fw_actions and self.y <= W-1:
+                        s_primes[idx][:] = np.array([self.x, self.y+1, h])
+                    elif a in Action.bw_actions and self.y > 0:
+                        s_primes[idx][:] = np.array([self.x, self.y-1, h])
+                    else:
+                        s_primes[idx][:] = np.array([self.x, self.y, h])
+                elif h in LEFT:
+                    if a in Action.fw_actions and self.x > 0:
+                        s_primes[idx][:] = np.array([self.x-1, self.y, h])
+                    elif a in Action.bw_actions and self.x < L-1:
+                        s_primes[idx][:] = np.array([self.x+1, self.y, h])
+                    else:
+                        s_primes[idx][:] = np.array([self.x, self.y, h])
+                elif h in RIGHT:
+                    if a in Action.fw_actions and self.x <= L-1:
+                        s_primes[idx][:] = np.array([self.x+1, self.y, h])
+                    elif a in Action.bw_actions and self.x > 0:
+                        s_primes[idx][:] = np.array([self.x-1, self.y, h])
+                    else:
+                        s_primes[idx][:] = np.array([self.x, self.y, h])
+                elif h in DOWN:
+                    if a in Action.fw_actions and self.y > 0:
+                        s_primes[idx][:] = np.array([self.x, self.y-1, h])
+                    elif a in Action.bw_actions and self.y <= W - 1:
+                        s_primes[idx][:] = np.array([self.x, self.y+1, h])
+                    else:
+                        s_primes[idx][:] = np.array([self.x, self.y, h])
+
+            #update candidate states with new heading if necessary     
+            if(a in Action.clk_actions):
+                s_primes[:,2] += 1
+            elif(a in Action.cclk_actions):
+                s_primes[:,2] += -1
+            s_primes[:,2] %= 12
+
+            #return nonzero chance if any candidates match the argument
+            for idx,row in enumerate(s_primes):
+                if s_new.x == row[0] and s_new.y == row[1] and s_new.h == row[2]:
+                    return chance[idx]
+                else:
+                    continue
+        return 0
+
+    def next_state_help(self, a, all_states):
+        pmf = []
+        pos = []
+        for st in all_states:
+            p = self.get_p(a, st)
+
+            if p > 0:
+                pmf.append(p)
+                pos.append(st)
+
+        idx = np.random.choice(len(pmf), p=pmf);
+        return pos[idx]
+                
 
 if __name__ == '__main__':
     env = Environment(W, L, rewards)
-    flattenStates = env.flattenStates()
-    print(flattenStates[10:20])
-    STM = np.array(
-        [[[(idx_k, idx_j, idx_k)  for (idx_k, a) in enumerate(Action.actions)] 
-            for (idx_j, s_prime) in enumerate(flattenStates)] 
-            for (idx_k, s) in enumerate(flattenStates)]
-    )
-    
-    env.printEnv()
-    # print('%r' % env.stateAt(1, 2))
-    print(transition_prob(STM, env.stateAt(1, 2), env.stateAt(2, 3), Action.UP))
+    print(env.next_state(Action.FORWARD_CLK))
+
