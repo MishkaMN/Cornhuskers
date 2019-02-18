05e0f6b56046ccfb5e3833f75668862ff15e7eee
Li-Wei Chi <weberchichi@gmail.com>
2019-02-17 16:22:13 -0800

    git pull from visualizer

--- lab3/env.py
+++ lab3/env.py
@@ -192,8 +192,8 @@ class Environment:
 
     def get_init_policy(self):
         # populate an init policy that moves closer to goal state
-        init_policy = [ [ [ self.action_to_take(self.stateAt(x, y, h), self.goal_state)
-            for x in range(L) ]for y in range(W)] for h in headings]
+        init_policy = np.array([ [ [ self.action_to_take(self.stateAt(x, y, h), self.goal_state)
+            for x in range(L) ]for y in range(W)] for h in headings])
         
         return init_policy
 
@@ -262,12 +262,14 @@ class Environment:
         pmf = []
         pos = []
         for st in self.flattenStates():
-            p = self.get_p(State(self.x, self.y, self.heading, -1), st, a)
+            p = self.get_p(State(self.robot.x, self.robot.y, self.robot.heading, -1), st, a)
 
             if p > 0:
                 pmf.append(p)
                 pos.append(st)
 
+        print('pmf: ', pmf)
+        print('pos: ', pos)
         idx = np.random.choice(len(pmf), p=pmf);
         return pos[idx]
 
@@ -276,6 +278,15 @@ class Environment:
         for st in self.flattenStates():
             
             p = self.get_p(state, st, policy[state.heading][state.y][state.x])
+
+            if p > 0:
+                possible_states.append((st, p))
+        return possible_states
+
+    def get_possible_states_from_action(self, state, action):
+        possible_states = []
+        for st in self.flattenStates():
+            p = self.get_p(state, st, action)
 
             if p > 0:
                 possible_states.append((st, p))
@@ -296,13 +307,34 @@ class Environment:
         # value at state with depth and discount factor gamma
         value = 0
         next_states = self.get_possible_next_states(state, policy)
-        #print('current state: ', state)
+        print('next_state: ', next_states)
         for (next_state, prob) in next_states:
-            #print('next_state: ', next_state)
-            #print('prob: ', prob)
-            #print('discount factor: ', (gamma**depth))
             value += prob*state.reward+(gamma**depth)*(self.policy_eval(next_state, \
                 policy, gamma, depth=depth+1))
 
         return value
 
+    def find_optimal_policy(self, init_policy, gamma):
+        optimal_policy = init_policy
+        optimal_values = np.zeros(optimal_policy.shape)
+
+        for h_idx, h_layer in enumerate(self.states):
+            for y_idx, y_row in enumerate(h_layer):
+                for x_idx, s in enumerate(y_row): 
+                    outcomes = []
+                    # pick the best action with maximum expected reward
+                    for a in actions:
+                        value = 0
+                        possible_states = self.get_possible_states_from_action(s, a)
+                        # print(possible_states)
+                        for new_s, prob in possible_states:
+                            value += prob*(new_s.reward+gamma*self.policy_eval(new_s, optimal_policy, gamma))
+                            outcomes.append(value)
+                    action_idx = np.argmax(outcomes)
+                    if action_idx == 0:
+                        action_idx += 1
+
+                    optimal_values[h_idx][y_idx][x_idx] = outcomes[action_idx]
+                    optimal_policy[h_idx][y_idx][x_idx] = actions[action_idx]
+                    print("{}, {}, {}, {}".format(s.x,s.y,s.heading,optimal_policy[h_idx][y_idx][x_idx]))
+        return optimal_policy, optimal_values
--- lab3/mdp_system.py
+++ lab3/mdp_system.py
@@ -3,7 +3,7 @@ from utils import *
 from env import *
 from robot import *
 from visualizer import Visualizer
-
+import time
 
 def calc_reward(env, inputs):
     # inputs are states that are traversed
@@ -26,26 +26,31 @@ if __name__ == '__main__':
     ]
     gamma = 0.9
 
-    robot = Robot(1, 4, 6, .1)
+    robot = Robot(1, 4, 6, 0)
     env = Environment(W, L, rewards, robot)
     
+    start = time.time()
     policy = env.get_init_policy()
     
     seq = [(robot.x, robot.y, robot.heading)]
     vis = Visualizer(seq)
     vis.showPolicy(policy)
 
+    global opt_policy
+    global opt_values
+    opt_policy, opt_vals = env.find_optimal_policy(policy, gamma)
+    # print(time.time()-start)
     route = []
-    # for i in range(10):
-        # action = policy[env.robot.heading][env.robot.y][env.robot.x]
-        # print('action:', action)
-        # next_state = env.get_next_state(action)
-        # print('next_state: ', next_state)
-        # env.robot.move(next_state.x, next_state.y, next_state.heading)
-        # print('robot pos: ', env.robot.x, env.robot.y, env.robot.heading)
-        # route.append((env.robot.x, env.robot.y, env.robot.heading))
-    # print(env.policy_eval(State(robot.x, robot.y, robot.heading, 0),
-    #    policy, gamma))
+
+    for i in range(10):
+        action = opt_policy[env.robot.heading][env.robot.y][env.robot.x]
+        print('action:', action)
+        next_state = env.get_next_state(action)
+        print('next_state: ', next_state)
+        env.robot.move(next_state.x, next_state.y, next_state.heading)
+        print('robot pos: ', env.robot.x, env.robot.y, env.robot.heading)
+        route.append((env.robot.x, env.robot.y, env.robot.heading))
+
     # seq = [(robot.x, robot.y, robot.heading)]
     # for i in range(10):
     #     action = policy[env.robot.heading][env.robot.y][env.robot.x]
