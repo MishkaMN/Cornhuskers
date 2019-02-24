5e1e5413618d026ab63c4a4fd4ead198bcd31d10
Li-Wei Chi <weberchichi@gmail.com>
2019-02-23 17:39:53 -0800

    tree working (no collision detection yet)

--- lab4/rrt.py
+++ lab4/rrt.py
@@ -1,44 +1,12 @@
-
 import numpy as np
-import time
-import pickle
-from utils import *
 from env import *
-from robot import *
-from visualizer import Visualizer
-import pdb
 import math
-
-delta = 10 # Distance the robot can run for 1sec.
-
-
-
-def get_control_input(s1,s2):
-    #2.2c
-
-def sim_step_from_to(s1, s2):
-	"""
-    2.2c
-	This func makes the robot step 1 sec to target state from initial state
-	Args:
-		p1: initial state
-		p2: target state (with any heading because only when physically turning we use theta)
-	Returns:
-		p: actual state ended with
-	"""
-	if dist(s1,s2) < delta:
-        return s2
-    else:
-        theta = atan2(s2.y-s1.y,s2.x-s1.x)
-        return s1.x + delta*cos(theta), s1.y + delta*sin(theta)
 
 def collides(s):    #check if state/point collides with the obstacle
     for rect in rectObs:
         if rect.collidepoint(p) == True:
             return True
     return False
-
-
 
 # Return Euclidean distance between two States
 def dist(s0, s1):
@@ -52,7 +20,7 @@ def nearestNeighbors(V, target):
 
     # Each point is a State object
     for point in V:
-        curr_distance = distance(point, target)
+        curr_distance = dist(point, target)
         if min_distance is None:
             res.append(point)
             min_distance = curr_distance
