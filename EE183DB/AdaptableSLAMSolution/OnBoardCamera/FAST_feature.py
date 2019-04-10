bbcd7ae42fea5b70afaa49a716cf9daf54f8c7c3
MishkaMN <misheel921@g.ucla.edu>
2019-04-10 01:55:19 -0700

    Update FAST_feature.py

--- EE183DB/AdaptableSLAMSolution/OnBoardCamera/FAST_feature.py
+++ EE183DB/AdaptableSLAMSolution/OnBoardCamera/FAST_feature.py
@@ -1,48 +1,43 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('simple2.jpg',0)
-import cv2.aruco as aruco
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
  
- 
-cap = cv.VideoCapture(0)
- 
-while(True):
-	# Capture frame-by-frame
-	ret, img = cap.read()
+# we used 4.0.1 opencv
+print(cv.__version__)
+# initialize the camera
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 32
+# grab a reference to the raw camera capture
+rawCapture = PiRGBArray(camera,size=(640,480))
 
-	# Initiate FAST object with default values
-	fast = cv.FastFeatureDetector_create()
+ # allow the camera to warmup
+time.sleep(0.1)
 
-	# Try to change threshold:
-	fast.setThreshold(10)
 
-	# Try to get name?
-	print( fast.getDefaultName())
+for frame in camera.capture_continuous(rawCapture, format ="bgr", use_video_port = True):
+    # get a frame
+    img = frame.array
 
-	# find and draw the keypoints
-	kp = fast.detect(img,None)
+    # init fast detector
+    fast = cv.FastFeatureDetector_create()
 
-	img2 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
+    # pixel intensity difference threshold. if more than this threshold, it is picked up as a feature
+    fast.setThreshold(10)
 
-	# Print all default params
-	# print( "Threshold: {}".format(fast.getThreshold()) )
-	# print( "nonmaxSuppression:{}".format(fast.getNonmaxSuppression()) )
-	# print( "neighborhood: {}".format(fast.getType()) )
-	# print( "Total Keypoints with nonmaxSuppression: {}".format(len(kp)) )
-	# cv.imwrite('fast_true2.png',img2)
+    # find and draw the keypoints
+    kp = fast.detect(img,None)
+    img2 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
 
-	# # Disable nonmaxSuppression
-	# fast.setNonmaxSuppression(0)
-	# kp = fast.detect(img,None)
-	# print( "Total Keypoints without nonmaxSuppression: {}".format(len(kp)) )
-	# img3 = cv.drawKeypoints(img, kp, None, color=(255,0,0))
-	# cv.imwrite('fast_false2.png',img3)
+    # show the video
+    cv.imshow('frame',img2)
 
-	cv.imshow('frame',img2)
-	if cv.waitKey(1) & 0xFF == ord('q'):
-		break
- 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+    # q to quit
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    # Clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
