c2222d3c9b194c70e2c3ef8c13e770904f7e1cb7
Ayush Rath <ayush.rath@gmail.com>
2019-04-10 14:28:49 -0700

    add comments to state estimator

--- EE183DB/AdaptableSLAMSolution/OverheadCameraTracking/WebcamStateEst.py
+++ EE183DB/AdaptableSLAMSolution/OverheadCameraTracking/WebcamStateEst.py
@@ -9,17 +9,18 @@ while(True):
 
     ret, frame = cap.read()
 
+    #detect aruco tags and find corners
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     parameters =  aruco.DetectorParameters_create()
- 
-
     corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 
+    #helper function: find center of aruco tag from corners
     def getCenter(corners, i, j):
         center = (int((corners[i][j][0][0] + corners[i][j][1][0] + corners[i][j][2][0] + corners[i][j][3][0])/4), int((corners[i][j][0][1] + corners[i][j][1][1] + corners[i][j][2][1] + corners[i][j][3][1])/4))
         return center
 
+    #helper function: find center and direction vector from aruco tag
     def getPose(corners, pMatrix):        
         center = np.array(getCenter(corners, 4, 0), dtype='float32')
         topCenter = np.array( ((corners[4][0][0][0] + corners[4][0][1][0])/2, (corners[4][0][0][1] + corners[4][0][1][1])/2) , dtype='float32')
@@ -34,22 +35,28 @@ while(True):
     if (not ids is None) and (len(ids) == 5):
         sortedCorners =  [x for _,x in sorted(zip(ids,corners))]
         
+        #find centers of environment tags
         topLeft = getCenter(sortedCorners, 0, 0)
         topRight = getCenter(sortedCorners, 1, 0)
         bottomLeft = getCenter(sortedCorners, 2, 0)
         bottomRight = getCenter(sortedCorners, 3, 0)
 
+        #Perspective transform to correct for angle of camera
         pts1 = np.float32([topLeft, topRight, bottomLeft, bottomRight])
         pts2 = np.float32([[0,0],[envSize,0],[0,envSize],[envSize,envSize]])
         M = cv2.getPerspectiveTransform(pts1,pts2)
+
+        #perform pose estimates
         center, topCenter, vec = getPose(sortedCorners,M)
         center = (center[0], envSize - center[1])
         angle = np.arctan2(-1*vec[1], vec[0])
         print(center, angle*180/np.pi)
+
+        #draw frames
         frame = cv2.warpPerspective(frame,M,(envSize,envSize))
         cv2.line(frame, (int(center[0]), int(center[1])), (int(topCenter[0]), int(topCenter[1])), (0,255,0), 3)
-
         cv2.imshow('frame',frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
