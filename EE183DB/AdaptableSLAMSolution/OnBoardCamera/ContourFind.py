a78041e7d99e7486c4842c9f45d08a102b799c29
Ayush Rath <ayush.rath@gmail.com>
2019-05-19 15:27:49 -0700

    update Contourfind

--- EE183DB/AdaptableSLAMSolution/OnBoardCamera/ContourFind.py
+++ EE183DB/AdaptableSLAMSolution/OnBoardCamera/ContourFind.py
@@ -1,5 +1,5 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+#from picamera.array import PiRGBArray
+#from picamera import PiCamera
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -19,14 +19,16 @@ def locateObstacle(img):
     lower_blue = np.array([90,50,50])
     upper_blue = np.array([110,255,255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
     isolated_blue = cv2.bitwise_and(img,img, mask= mask)
+    cv2.imshow("mask", isolated_blue)
     _, threshold = cv2.threshold(isolated_blue, 80, 255, cv2.THRESH_BINARY)
     imgray = cv2.cvtColor(threshold, cv2.COLOR_BGR2GRAY);
     contours, hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Find the index of the largest contour
     areas = np.array([cv2.contourArea(c) for c in contours])
-    cnts = [contours[i] for i in np.where(areas > 10000)[0]]
+    cnts = [contours[i] for i in np.where(areas > 1000)[0]]
 
     locations = []
     for cnt in cnts:
@@ -49,57 +51,57 @@ def main():
     input("Press Enter to Start")
 
     #cap = cv2.VideoCapture('LMarkVideo.mp4')
-    camera = PiCamera()
-    camera.vflip = True
-    rawCap = PiRGBArray(camera)
-    time.sleep(1)
+    # camera = PiCamera()
+    # camera.vflip = True
+    # rawCap = PiRGBArray(camera)
+    # time.sleep(1)
     flag=True
-    while(1):
-        startTime = time.time()
-        #ret, img = cap.read()
-        camera.capture(rawCap, format="bgr")
-        img = rawCap.array
-        locations = locateObstacle(img)
+    # while(1):
+    #     startTime = time.time()
+    #     #ret, img = cap.read()
+    #     camera.capture(rawCap, format="bgr")
+    img = cv2.imread("tmp.jpg")
+    locations = locateObstacle(img)
 
-        objPoseX = []
-        objPoseY = []
-        print("Frame:")
-        for loc in locations:
-            #print(angle*180/np.pi)
-            d2 = loc[0]
-            angle = loc[1]
-            print(d2,angle)
-            x = loc[2]
-            y = loc[3]
-            w = loc[4]
-            h = loc[5]
-            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.putText(img, "angle"+str(angle*180/np.pi), (x, y), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,255,0), lineType=cv2.LINE_AA) 
-            cv2.putText(img, "Dist"+str(d2), (x, y-100), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,255,0), lineType=cv2.LINE_AA)
-            objPoseX.append(d2*np.sin(angle))
-            objPoseY.append(d2*np.cos(angle))
+    objPoseX = []
+    objPoseY = []
+    print("Frame:")
+    for loc in locations:
+        #print(angle*180/np.pi)
+        d2 = loc[0]
+        angle = loc[1]
+        print(d2,angle)
+        x = loc[2]
+        y = loc[3]
+        w = loc[4]
+        h = loc[5]
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(img, "angle"+str(angle*180/np.pi), (x, y), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,255,0), lineType=cv2.LINE_AA) 
+        cv2.putText(img, "Dist"+str(d2), (x, y-100), cv2.FONT_HERSHEY_TRIPLEX, 2, (0,255,0), lineType=cv2.LINE_AA)
+        objPoseX.append(d2*np.sin(angle))
+        objPoseY.append(d2*np.cos(angle))
 
-        #plt.cla()
-        #plt.scatter(objPoseX, objPoseY)
-        bottom, top = plt.ylim()
-        fovDistX = np.max(plt.xlim())
-        #plt.xlim(-1.05*fovDistX,1.05*fovDistX)
-        #plt.ylim(0-top*.05,top*1.05)
-        #plt.scatter([0],[0], color="red")
+    #plt.cla()
+    #plt.scatter(objPoseX, objPoseY)
+    bottom, top = plt.ylim()
+    fovDistX = np.max(plt.xlim())
+    #plt.xlim(-1.05*fovDistX,1.05*fovDistX)
+    #plt.ylim(0-top*.05,top*1.05)
+    #plt.scatter([0],[0], color="red")
 
-        #for i,_ in enumerate(objPoseX):
-            #plt.plot(np.array((0, objPoseX[i])),np.array((0, objPoseY[i])), '--r')
-        #cv2.imshow("shapes", img)
-        cv2.imwrite("tmp.jpg", img)
-        
-        #plt.pause(0.000001)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    break
-        if flag:
-            input("Press Enter to Continue")
-            flag=False
-            exit()
-        rawCap.truncate(0)
+    #for i,_ in enumerate(objPoseX):
+        #plt.plot(np.array((0, objPoseX[i])),np.array((0, objPoseY[i])), '--r')
+    cv2.imshow("shapes", img)
+    #cv2.imwrite("tmp.jpg", img)
+    
+    #plt.pause(0.000001)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
+    if flag:
+        input("Press Enter to Continue")
+        flag=False
+        exit()
+    #rawCap.truncate(0)
 #        print(time.time()-startTime, "Processing Time")
 
 
